141112cd38acd4b057b16d11ae7e786ff8c6728b
machida-being <machida@beincorp.co.jp>
2021-11-19 09:24:58 +0900

    rubyファイルの追加

---
+++ main.rb
@@ -0,0 +1,68 @@
+# frozen_string_literal: true
+
+require 'sinatra'
+require 'sinatra/reloader'
+require 'json'
+
+helpers do
+  def h(text)
+    Rack::Utils.escape_html(text)
+  end
+end
+
+def load(_)
+  File.open(FILE_PATH) { |f| JSON.load(f) }
+end
+
+FILE_PATH = 'data/data.json'
+FILE_PATH.freeze
+
+get '/memos' do
+  if File.exist?(FILE_PATH)
+    @memos = load(FILE_PATH)
+  else
+    File.open(FILE_PATH, 'w') { |f| f.write('{}') }
+  end
+  @memos = load(FILE_PATH)
+  erb :top
+end
+
+post '/memos' do
+  @memos = load(FILE_PATH)
+  @memos[SecureRandom.uuid] = { 'title' => params['title'], 'content' => params['content'] }
+  File.open(FILE_PATH, 'w') { |f| JSON.dump(@memos, f) }
+  redirect to '/memos'
+end
+
+get '/memos/:id/details' do
+  memos = load(FILE_PATH)
+  @id = params[:id]
+  @memo = memos[@id]
+  erb :show
+end
+
+get '/new' do
+  erb :new
+end
+
+get '/memos/:id/edit' do
+  memos = load(FILE_PATH)
+  @id = params[:id]
+  @memo = memos[@id]
+  erb :edit
+end
+
+patch '/memos/:id' do
+  id = params[:id]
+  @memos = load(FILE_PATH)
+  @memos[id] = { 'title' => params['title'], 'content' => params['content'] }
+  File.open(FILE_PATH, 'w') { |f| JSON.dump(@memos, f) }
+  redirect to '/memos'
+end
+
+delete '/memos/:id' do
+  @memos = load(FILE_PATH)
+  @memos.delete(params[:id])
+  File.open(FILE_PATH, 'w') { |f| JSON.dump(@memos, f) }
+  redirect to '/memos'
+end
