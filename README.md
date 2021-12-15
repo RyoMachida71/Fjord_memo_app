d0be9d9c547f085272ed156c7f6d6b02662a6dcc
machida-being <machida@beincorp.co.jp>
2021-12-15 10:43:40 +0900

    添削の修正

--- README.md
+++ README.md
@@ -1,7 +1,46 @@
 # 概要
 sinatraを使用して簡易なメモアプリを作成しました。
 
-# 手順
+# DBのセットアップ手順
+Postgre SQLがすでにインストールされていることを前提とする。
+
+①psqlにログインする
+
+`psql -U <ユーザ名>`
+
+②以下のコマンドでmemosというDBを作成する
+
+`CREATE DATABASE memos`
+
+③memosデータベースに接続する
+
+`psql -U <ユーザ名> -d <データベース名>`
+
+④データテーブルを作成する
+
+`CREATE TABLE memos_data (id TEXT, title TEXT, content TEXT)`
+
+# 環境変数の設定と利用方法
+DBに接続するためのユーザ名やパスワードを環境変数として設定しておく
+
+①Gemfileに以下を追加する
+
+`gem 'dotenv'`
+
+②.envファイルを作成し、.gitignoreに追加する
+
+
+③.envファイルに以下のように変数を宣言する
+
+`DB_USER = "DBを作成した時のユーザ名"`
+`DB_PASSWORD = "DBを作成した時のパスワード"`
+
+④main.rbファイルで環境変数を利用する
+
+`Dotenv.load`
+`PG.connect(host: 'localhost', user: ENV['DB_USER'], password: ENV['DB_PASSWORD'], dbname: 'memos')`
+
+# アプリケーション実行手順
 ①`git clone`でリポジトリをローカルにクローンする
 
 `git clone https://github.com/machida-being/Fjord_memo_app`
@@ -14,8 +53,11 @@ sinatraを使用して簡易なメモアプリを作成しました。
 
 `bundle install`
 
-④`main.rb`を以下のコマンドで実行する 
+④環境変数と利用方法の項目を実行する
+
+
+⑤`main.rb`を以下のコマンドで実行する 
 
 `bundle exec ruby main.rb`
 
-⑤`http://localhost/memos`にアクセスする <br>
+⑥`http://localhost:4567/memos`にアクセスする <br>
--- main.rb
+++ main.rb
@@ -4,41 +4,39 @@ require 'sinatra'
 require 'sinatra/reloader'
 require 'json'
 require 'pg'
+require 'dotenv'
 
 helpers do
   def h(text)
     Rack::Utils.escape_html(text)
   end
 end
-CONNECTION = PG.connect(host: 'localhost', user: 'postgres', password: 'Pu6kgmth', dbname: 'memos')
-CONNECTION.freeze
 
-def create_table
-  CONNECTION.exec('CREATE TABLE IF NOT EXISTS memos_data (id TEXT PRIMARY KEY, title TEXT, content TEXT)')
-end
+Dotenv.load
+CONNECTION = PG.connect(host: 'localhost', user: ENV['DB_USER'], password: ENV['DB_PASSWORD'], dbname: 'memos')
+CONNECTION.freeze
 
 def read
   CONNECTION.exec('SELECT * FROM memos_data')
 end
 
 def write(id, title, content)
-  CONNECTION.exec("INSERT INTO memos_data (id, title, content) VALUES('#{id}', '#{title}', '#{content}')")
+  CONNECTION.exec('INSERT INTO memos_data (id, title, content) VALUES($1, $2, $3);', [id, title, content])
 end
 
 def get_record(id)
-  CONNECTION.exec("SELECT * FROM memos_data WHERE id='#{id}'")
+  CONNECTION.exec('SELECT * FROM memos_data WHERE id=$1;', [id])
 end
 
 def rewrite(id, title, content)
-  CONNECTION.exec("UPDATE memos_data SET (title, content) = ('#{title}', '#{content}') WHERE id = '#{id}'")
+  CONNECTION.exec('UPDATE memos_data SET (title, content) = ($1, $2) WHERE id = $3;', [title, content, id])
 end
 
 def delete(id)
-  CONNECTION.exec("DELETE FROM memos_data WHERE id = '#{id}'")
+  CONNECTION.exec('DELETE FROM memos_data WHERE id = $1;', [id])
 end
 
 get '/memos' do
-  create_table
   @memos = read
   erb :top
 end
